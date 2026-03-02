ed073986b5e425b8d3514f785c32ed2b417912f9
Angelo Junior <121700150+angelosousajr@users.noreply.github.com>
2026-03-02 11:28:25 -0300

    Add files via upload

---
+++ ExercMatrizes/MatrizAsteriscos.java
@@ -0,0 +1,11 @@
+public class MatrizAsteriscos {
+    public static void main(String[] args) {
+        int [][] m = new int[4][4];
+        for(int n = 0; n < 4; n++){
+            for(int i = 0; i < 4; i++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
