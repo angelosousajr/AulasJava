87693dc2ee673fe7b25fb668644ee0ac12286ff7
Angelo Junior <121700150+angelosousajr@users.noreply.github.com>
2026-03-17 09:23:07 -0300

    Display matrix and diagonal elements
    Added matrix display functionality and improved output formatting.

--- ListaMatrizes/ListaMatriz04A.java
+++ ListaMatrizes/ListaMatriz04A.java
@@ -9,6 +9,16 @@ public class ListaMatriz04A {
             }
         }
 
+        System.out.println("");
+        System.out.println("Matriz:");
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                System.out.printf("%.2f  ", m[i][j]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println("");
         System.out.println("Elementos da diagonal principal:");
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
