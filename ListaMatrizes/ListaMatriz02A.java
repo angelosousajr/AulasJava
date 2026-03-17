a67ee89d3fec4f6012a21a02a6cdafb3b6b6ef0d
Angelo Junior <121700150+angelosousajr@users.noreply.github.com>
2026-03-17 09:20:54 -0300

    Update ListaMatriz02A.java

--- ListaMatrizes/ListaMatriz02A.java
+++ ListaMatrizes/ListaMatriz02A.java
@@ -7,9 +7,9 @@ public class ListaMatriz02A {
             for(int j = 0; j < 5; j++){
                 m[i][j] = (int)(Math.random()*9)+1;
             }
-            System.out.println("");
         }
 
+        System.out.println("");
         System.out.println("Matriz:");
         for (int i = 0; i < 5; i++){
             for (int j = 0; j < 5; j++){
@@ -18,6 +18,7 @@ public class ListaMatriz02A {
             System.out.println("");
         }
 
+        System.out.println("");
         System.out.println("Soma dos números impares:");
         int somaImpares = 0;
         for(int i = 0; i < 5; i++){
