81867ac26a094417c29bf65af38896d111019214
Angelo Junior <121700150+angelosousajr@users.noreply.github.com>
2026-03-16 21:11:41 -0300

    Fix array initialization syntax in ListaMatriz04A

--- ListaMatrizes/ListaMatriz04A.java
+++ ListaMatrizes/ListaMatriz04A.java
@@ -1,6 +1,6 @@
 public class ListaMatriz04A {
     public static void main(String[] args){
-        double [][] m = double int[4][4];
+        double [][] m = new double [4][4];
 
         System.out.println("Sorteando números decimais...");
         for(int i = 0; i < 4; i++){
