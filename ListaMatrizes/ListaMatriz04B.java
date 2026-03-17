18afa5039e7672cf59e7c3457719753fc27e0c72
Angelo Junior <121700150+angelosousajr@users.noreply.github.com>
2026-03-16 21:05:46 -0300

    Refactor to ListaMatriz04B for decimal matrix handling

--- ListaMatrizes/ListaMatriz04B.java
+++ ListaMatrizes/ListaMatriz04B.java
@@ -1,11 +1,11 @@
-public class ListaMatriz04A {
+public class ListaMatriz04B {
     public static void main(String[] args){
-        int [][] m = new int[4][4];
+        double [][] m = double int[4][4];
 
-        System.out.println("Sorteando números...");
-        for(double i = 0; i < 4; i++){
-            for(double j = 0; j < 4; j++){
-                m[i][j] = (double)(Math.random()*9)+1;
+        System.out.println("Sorteando números decimais...");
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                m[i][j] = (Math.random()*9)+1;
             }
         }
 
@@ -13,13 +13,13 @@ public class ListaMatriz04A {
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
                 if (i == 0 && j == 3){
-                    System.out.println(m[i][j]);
+                    System.out.println("%.2f  ", m[i][j]);
                 }   else if(i == 1 && j == 2){
-                    System.out.println(m[i][j]);
+                    System.out.println("%.2f  ", m[i][j]);
                 }   else if(i == 2 && j == 1){
-                    System.out.println(m[i][j]);
+                    System.out.println("%.2f  ", m[i][j]);
                 }   else if(i == 3 && j == 0){
-                    System.out.println(m[i][j]);
+                    System.out.println("%.2f  ", m[i][j]);
                 }
             }
             System.out.println("");
