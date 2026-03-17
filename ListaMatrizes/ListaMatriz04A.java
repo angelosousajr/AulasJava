b6a80fbd313ab33b15e0e0d036228e1621587131
Angelo Junior <121700150+angelosousajr@users.noreply.github.com>
2026-03-16 21:05:20 -0300

    Change matrix to double and update output format
    Updated the matrix to use double type and modified output format.

--- ListaMatrizes/ListaMatriz04A.java
+++ ListaMatrizes/ListaMatriz04A.java
@@ -1,11 +1,11 @@
 public class ListaMatriz04A {
     public static void main(String[] args){
-        int [][] m = new int[4][4];
+        double [][] m = double int[4][4];
 
-        System.out.println("Sorteando números...");
-        for(double i = 0; i < 4; i++){
-            for(double j = 0; j < 4; j++){
-                m[i][j] = (double)(Math.random()*9)+1;
+        System.out.println("Sorteando números decimais...");
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                m[i][j] = (Math.random()*9)+1;
             }
         }
 
@@ -13,7 +13,7 @@ public class ListaMatriz04A {
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
                 if(i == j){
-                    System.out.println(m[i][j]);
+                    System.out.println("%.2f  ", m[i][j]);
             }
         }
         System.out.println("");
